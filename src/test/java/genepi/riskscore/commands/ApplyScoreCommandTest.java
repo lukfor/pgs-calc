d1b6d11afbce160f4a731ccab0304968b92bedfa
lukfor <lukas.forer@i-med.ac.at>
2020-03-13 16:34:50 +0100

    Add test to ensure output file is written correctly

--- src/test/java/genepi/riskscore/commands/ApplyScoreCommandTest.java
+++ src/test/java/genepi/riskscore/commands/ApplyScoreCommandTest.java
@@ -2,6 +2,8 @@ package genepi.riskscore.commands;
 
 import static org.junit.Assert.assertEquals;
 
+import java.io.IOException;
+
 import org.junit.Test;
 
 import genepi.io.table.reader.CsvTableReader;
@@ -15,8 +17,11 @@ public class ApplyScoreCommandTest {
 	@Test
 	public void testCall() {
 
-		/*String[] args = { "--chr", "20", "--vcf", "test-data/chr20.dose.vcf.gz", "--ref", "/Users/lukas/Downloads/Khera.et.al_GPS_BMI_Cell_2019.txt",
-				"--out", "output.csv" };*/
+		/*
+		 * String[] args = { "--chr", "20", "--vcf", "test-data/chr20.dose.vcf.gz",
+		 * "--ref", "/Users/lukas/Downloads/Khera.et.al_GPS_BMI_Cell_2019.txt", "--out",
+		 * "output.csv" };
+		 */
 		String[] args = { "--chr", "20", "--vcf", "test-data/chr20.dose.vcf.gz", "--ref", "test-data/chr20.scores.csv",
 				"--out", "output.csv" };
 		int result = new CommandLine(new ApplyScoreCommand()).execute(args);
@@ -26,11 +31,42 @@ public class ApplyScoreCommandTest {
 		ITableReader reader = new CsvTableReader("output.csv", ',');
 		while (reader.next()) {
 			samples++;
-			// TODO: compare with expected results
 
 		}
 		assertEquals(EXPECTED_SAMPLES, samples);
 
+	}
+
+	@Test
+	public void testCallAndCheckOutputFile() throws IOException {
+
+		String[] args = { "--chr", "20", "--vcf", "test-data/two.vcf", "--ref", "test-data/chr20.scores.csv", "--out",
+				"output.csv" };
+		int result = new CommandLine(new ApplyScoreCommand()).execute(args);
+		assertEquals(0, result);
+
+		ITableReader reader = new CsvTableReader("output.csv", ',');
+
+		assertEquals(true, reader.next());
+
+		double score = reader.getDouble("score");
+		String sample = reader.getString("sample");
+		String chr = reader.getString("chr");
+		assertEquals("20", chr);
+		assertEquals("LF001", sample);
+		assertEquals(0.4, score, 0.0000001);
+
+		assertEquals(true, reader.next());
+
+		score = reader.getDouble("score");
+		sample = reader.getString("sample");
+		chr = reader.getString("chr");
+		assertEquals("20", chr);
+		assertEquals("LF002", sample);
+		assertEquals(1, score, 0.0000001);
+
+		assertEquals(false, reader.next());
+		reader.close();
 	}
 
 	@Test
