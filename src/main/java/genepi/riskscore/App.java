5f7d0b13b3617ad659eab26c88d46267a9511c3d
Lukas Forer <lukas.forer@i-med.ac.at>
2020-05-13 11:31:00 +0200

    Add copyright and url to software header

--- src/main/java/genepi/riskscore/App.java
+++ src/main/java/genepi/riskscore/App.java
@@ -10,10 +10,20 @@ public class App {
 
 	public static final String VERSION = "0.6.0";
 
+	public static final String URL = "https://github.com/lukfor/riskscore";
+	
+	public static final String COPYRIGHT = "(c) 2020 Lukas Forer, Claudia Lamina and Stefan Coassin";
+	
 	public static void main(String[] args) {
 		
 		System.out.println();
 		System.out.println(APP + " "+ VERSION);
+		if (URL != null && !URL.isEmpty()) {
+			System.out.println(URL);
+		}
+		if (COPYRIGHT != null &&! COPYRIGHT.isEmpty()) {
+			System.out.println(COPYRIGHT);
+		}
 		System.out.println();
 		
 		new CommandLine(new ApplyScoreCommand()).execute(args);
