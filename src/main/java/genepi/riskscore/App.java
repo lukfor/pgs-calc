43b9cf8433cadcd9b13ff981869f94c600af55a7
Lukas Forer <lukas.forer@i-med.ac.at>
2021-10-05 16:01:27 +0200

    Prepare release 0.9.5

--- src/main/java/genepi/riskscore/App.java
+++ src/main/java/genepi/riskscore/App.java
@@ -14,7 +14,7 @@ public class App {
 
 	public static final String APP = "pgs-calc";
 
-	public static final String VERSION = "0.9.4";
+	public static final String VERSION = "0.9.5";
 
 	public static final String URL = "https://github.com/lukfor/pgs-calc";
 
