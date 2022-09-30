84bdd03d433bd190e194b93923d5221c497af907
Lukas Forer <lukas.forer@i-med.ac.at>
2022-09-30 12:01:27 +0200

    Fix wrong path to test files

--- src/test/java/genepi/riskscore/commands/ResolveScoreCommandTest.java
+++ src/test/java/genepi/riskscore/commands/ResolveScoreCommandTest.java
@@ -105,7 +105,7 @@ public class ResolveScoreCommandTest {
 	
 	@Test
 	public void testResolvePRSWeb() {
-		String[] args = { "--in", "test-data/PRSWEB_PHECODE153_CRC-Huyghe_PT_UKB_20200608_WEIGHTS.txt", "--out", "test-data-output/PRSWEB_PHECODE153_CRC-Huyghe_PT_UKB_20200608_WEIGHTS.hg38.txt", "--dbsnp",
+		String[] args = { "--in", "test-data/PRSWEB_PHECODE153_CRC-Huyghe_PT_UKB_20200608.txt", "--out", "test-data-output/PRSWEB_PHECODE153_CRC-Huyghe_PT_UKB_20200608.hg38.txt", "--dbsnp",
 				DBSNP_INDEX, "--chain", "test-data/chains/hg19ToHg38.over.chain.gz" };
 		int result = new CommandLine(new ResolveScoreCommand()).execute(args);
 		assertEquals(0, result);
